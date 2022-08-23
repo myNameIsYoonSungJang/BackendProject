3589145b46fac3944944854dd48efeafbbc2b24e
YoongSung Jang <jang064880@gmail.com>
2022-08-23 22:14:56 +0900

    hp-21

--- src/main/java/com/example/jabdongsani/api/ArticleApiController.java
+++ src/main/java/com/example/jabdongsani/api/ArticleApiController.java
@@ -3,6 +3,7 @@ package com.example.jabdongsani.api;
 import com.example.jabdongsani.dto.ArticleForm;
 import com.example.jabdongsani.entity.Article;
 import com.example.jabdongsani.repository.ArticleRepository;
+import com.example.jabdongsani.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,59 +23,54 @@ import java.util.List;
 public class ArticleApiController {
 
     @Autowired // DI -> 디펜던시 인젝션 -> 외부에서 가져온다, 끌어온다.
-    private ArticleRepository articleRepository;
+    // DI -> 생성 객체를 가져와 연결.
+    private ArticleService articleService;
 
     // GET
     @GetMapping("api/articles")
     public List<Article> index(){
-        return articleRepository.findAll();
+        return articleService.index();
     }
     @GetMapping("api/articles/{id}")
-    public Article index(@PathVariable Long id){
-        return articleRepository.findById(id).orElse(null);
+    public Article show(@PathVariable Long id){
+        return articleService.show(id);
     }
     // POST
     @PostMapping("api/articles")
-    public Article create(@RequestBody ArticleForm dto){
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto){ // 상태코드 표현 가능! 같이 실어서 보낸다.
         // dto객체를 불러온다. 원래는 그냥 dto객체로 불러오면 된다.
         // 하지만 RestApi에서 JSON으로 던진 데이터는 그냥 받아지지 않는다.
         // @RequestBody 어노테이션을 이용해 http의 request 부분에서 body의 데이터를 불러와야 데이터를 받을수 있다.
-        Article article = dto.toEntity(); // DB에 저장하기 위해 entity 변환
-        return articleRepository.save(article); // DB에 저장
+        Article created = articleService.create(dto); // DB에 저장하기 위해 entity 변환
+        return (created != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(created) :  // DB에 저장
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // DB에 저장
+
     }
     // PATCH
     @PatchMapping("api/articles/{id}")
     //public Article update(@PathVariable Long id, @RequestBody ArticleForm dto){
     // 원래 코드는 이거지만 나중에 400 상태코드를 위해 데이터를 Article 타입으로 담아 보내줘야 한다.
     public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto){ // 상태코드 표현 가능! 같이 실어서 보낸다.
-        // 1. 수정용 엔티티 생성
-        Article article = dto.toEntity();
-        log.info("id : {}, article : {}", id, article.toString()); // 각각의 값이 {} 빈 괄호 안으로 대응된다.
-        // 2. 대상 엔티티 조회
-        Article target = articleRepository.findById(id).orElse(null);
-        // 3. 잘못된 요청 처리(대상이 없거나, id가 다른 경우)
-        if(target == null || id != article.getId()){
-            // 400, 잘못된 요청 응답
-            log.info("잘못된 요청! id : {}, article : {}", id, article.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 4. 업데이트 및 정상 응답(200)
-        target.patch(article);
-        Article updated = articleRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated); // 데이터의 정상적인 요청과 응답에는 body에 데이터를 실어서 보낸다.
+        Article updated = articleService.update(id, dto);
+        return (updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // 데이터의 정상적인 요청과 응답에는 body에 데이터를 실어서 보낸다.
     }
     //DELETE
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Article> delete(@PathVariable Long id){
-        // 대상 찾기
-        Article target = articleRepository.findById(id).orElse(null);
-        // 잘못된 요청 처리
-        if(target == null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 대상 삭제
-        articleRepository.delete(target);
-        // 데이터 반환
-        return ResponseEntity.status(HttpStatus.OK).build(); // body(null)로 줘도 되고 build()로 보내도 된다.
+        Article deleted = articleService.delete(id);
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build() : // body(null)로 줘도 되고 build()로 보내도 된다.
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+    // 트랜잭션 -> 실패하면? 롤백!
+    @PostMapping("/api/transaction-test")
+    public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos){
+        List<Article> createdList = articleService.createArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) : // body(null)로 줘도 되고 build()로 보내도 된다.
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
---
+++ src/main/java/com/example/jabdongsani/service/ArticleService.java
@@ -0,0 +1,94 @@
+package com.example.jabdongsani.service;
+
+import com.example.jabdongsani.dto.ArticleForm;
+import com.example.jabdongsani.entity.Article;
+import com.example.jabdongsani.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service // 서비스 선언 (서비스 객체를 스프링 부트에 생성)
+public class ArticleService {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    public Article show(Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    public Article create(ArticleForm dto) {
+        Article article = dto.toEntity();
+        if(article.getId() != null)
+            return null;
+        return articleRepository.save(article);
+    }
+
+    public Article update(Long id, ArticleForm dto) {
+        // 1. 수정용 엔티티 생성
+        Article article = dto.toEntity();
+        log.info("id : {}, article : {}", id, article.toString()); // 각각의 값이 {} 빈 괄호 안으로 대응된다.
+        // 2. 대상 엔티티 조회
+        Article target = articleRepository.findById(id).orElse(null);
+        // 3. 잘못된 요청 처리(대상이 없거나, id가 다른 경우)
+        if(target == null || id != article.getId()){
+            // 400, 잘못된 요청 응답
+            log.info("잘못된 요청! id : {}, article : {}", id, article.toString());
+            return null;
+        }
+        // 4. 업데이트
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return updated;
+    }
+
+    public Article delete(Long id) {
+        // 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+        // 잘못된 요청 처리
+        if(target == null){
+            return null;
+        }
+        // 대상 삭제
+        articleRepository.delete(target);
+        // 데이터 반환
+        return target;
+    }
+
+    @Transactional // 해당 메소드를 트랜잭션으로 묶는다. 하나 실패하면 롤백!
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        // 1. dto 묶음을 entity 묶음으로 변환
+        List<Article> articleList = dtos.stream().map(dto -> dto.toEntity()).collect(Collectors.toList()); // stream 문법
+        // stream 문법을 for문으로 작성했을 때의 코드
+//        List<Article> articleList = new ArrayList<>();
+//        for(int i = 0; i < dtos.size(); i++){
+//            ArticleForm dto = dtos.get(i);
+//            Article entity = dto.toEntity();
+//            articleList.add(entity);
+//        }
+
+        // 2. entity 묶음을 DB로 저장
+        articleList.stream().forEach(article -> articleRepository.save(article));
+         // stream 문법을 for문으로 작성했을 때의 코드
+//          for(int i = 0; i < articleList.size(); i++){
+//            Article article = articleList.get(i);
+//            articleRepository.save(article);
+//        }
+        // 3. 강제 예외 발생
+        articleRepository.findById(-1L).orElseThrow(
+                () -> new IllegalArgumentException("결제 실패!")
+        );
+        // 4. 결과값 반환
+        return articleList;
+    }
+}
